14141276ca788d56dc108779731f49e9ea165623
Furkan <furkan.bilgin@outlook.com>
2021-01-18 20:38:07 +0300

    Message send button

--- public/js/ui/send.js
+++ public/js/ui/send.js
@@ -1,8 +1,18 @@
-$("#chat-input").keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    
-    if (keycode == "13"){
+$(document).ready(() => {
+    function send() {
         window.main.connectionController.sendMessage($("#chat-input").val());
         $("#chat-input").val("");
     }
-});
+
+    $("#chat-input").keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        
+        if (keycode == "13"){
+            send();
+        }
+    });
+
+    $("#message-send-button").click(() => {
+        send();
+    });
+})
